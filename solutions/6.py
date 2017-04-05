6a967160772ba38f7589ee0aa99619e79ffdd432
Eval666 <playarammarket@gmail.com>
2017-04-05 20:54:20 +0300

    6.py
    Эффективно, но не профессионально.

--- solutions/6.py
+++ solutions/6.py
@@ -1,13 +1,4 @@
 
 
 def is_prime(number):
-    """Эту функцию можно сильно оптимизировать. Подумайте, как"""
-
-    if number == 1:
-        return False  # 1 - не простое число
-
-    for possible_divisor in range(2, number):
-        if number % possible_divisor == 0:
-            return False
-
-    return True
+    return (int(num)==num and num != 1)
